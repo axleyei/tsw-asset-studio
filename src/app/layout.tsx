db703d200d615110b3dfbfb118046c42f981afcb
axleyei <zuuldc@gmail.com>
2026-03-26 09:47:44 -0400

    Minor fixes to positioning. Added ico and apple touch icon.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,33 @@ import TopBar from '@/components/TopBar';
 
 const inter = Inter({ subsets: ['latin'] });
 
+const BASE_URL = 'https://tsw-asset-studio.vercel.app';
+
 export const metadata: Metadata = {
   title: 'TSW Asset Studio',
   description: 'Create image assets for The So What',
+  metadataBase: new URL(BASE_URL),
+  openGraph: {
+    title: 'TSW Asset Studio',
+    description: 'Create image assets for The So What',
+    url: BASE_URL,
+    siteName: 'TSW Asset Studio',
+    images: [
+      {
+        url: '/assets/example-evergreen.jpg',
+        width: 728,
+        height: 524,
+        alt: 'TSW Asset Studio',
+      },
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'TSW Asset Studio',
+    description: 'Create image assets for The So What',
+    images: ['/assets/example-evergreen.jpg'],
+  },
 };
 
 export default function RootLayout({
